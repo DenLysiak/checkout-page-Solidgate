4726e508b7bafb4abd1a3394f49574fe4e8b93ca
Denys Lysiak <Den@Denyss-Mac.local>
2025-06-07 22:04:38 +0300

    addaing new base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/checkout-page-Solidgate/',
   plugins: [react()],
 })
